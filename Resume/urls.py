6264a0c38e0532605e7c4e75ffd047a0445a5a75
Aashish Shrestha <itsmeasish98@gmail.com>
2019-09-18 20:07:54 +0545

    Navigation Bar

---
+++ Resume/urls.py
@@ -0,0 +1,31 @@
+"""Resume URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url
+
+from Resume import settings
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r"^$", views.HomePage.as_view(), name="home"),
+    # path('project/', include("projects.urls")),
+    url(r"^projects/", include(("projects.urls","projects"), namespace="projects")),
+]
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    url(r"^$", views.project_index, name="project_index"),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
+    # url(r"(?P<pk>\d+)/", views.project_detail, name="project_detail"),
+]
